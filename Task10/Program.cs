879fd6c92efb51382d055394d6f39dba2d66d40d
UrsAlex <ursalex@mail.ru>
2022-12-05 23:42:36 +0300

    решение задач(10, 13 , 15)

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine($"Введите число ");
+int number = int.Parse(Console.ReadLine());
+int numberA = (number / 10);
+int result = (numberA % 10);
+Console.WriteLine(result);
---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.WriteLine($"Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+
+
+if(number < 100) 
+    {
+    Console.WriteLine("Третей цифры нет ");
+    }
+    else
+        {while(number > 1000)
+            {
+                number = number / 10;  
+            }
+
+                int result = number % 10;
+                Console.WriteLine($"Третья цифра, {result}");
+        }    
+        
+
+     
